bb698be0866a7a904594dc7a1c294c0bf1e2dd39
Sean <e0406739@u.nus.edu>
2020-12-20 14:23:34 +0800

    figure commit issue 4

---
+++ src/contactus/contactusheader/ContactUsHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./styles.css";
+
+const ContactUsHeader = () => {
+  const ContactUsText = () => (
+    <span>
+      Do contact us if there is any error or improvement or wish to collaborate on this project
+    </span>
+  );
+
+  return (
+    <div className="contact-us-header-box">
+      <ContactUsHeader sectionHeader={'Contact Us'} translateX={'translateX(23px)'} />
+      <ContactUsText />
+    </div>
+  );
+};
+
+export default ContactUsHeader;
